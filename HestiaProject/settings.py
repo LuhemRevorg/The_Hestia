4cc78a90be6e004e5125c43c552f65fbeea9f46e
Mehul <mehulg8142@gmail.com>
2024-06-23 03:11:37 +0530

    h

--- HestiaProject/settings.py
+++ HestiaProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import psycopg2
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,12 +77,8 @@ WSGI_APPLICATION = 'HestiaProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'dcbvsngtvk',
-        'PASSWORD': 'Poopsalot69',
-        'HOST': 'the-hestia-server.postgres.database.azure.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
