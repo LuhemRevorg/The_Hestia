b4ba1fdf6414de8561c482a9ee85df90d8e49f0b
LuhemRevorg <148581376+LuhemRevorg@users.noreply.github.com>
2024-06-23 01:07:59 +0530

    Update settings.py

--- HestiaProject/settings.py
+++ HestiaProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4dez#*=fjdf)5@(ly2nq(--+cp2%j*tbk5i8^g&c59f8lb+87n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-hestia.azurewebsites.net']
 
 
 # Application definition
