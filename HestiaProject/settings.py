e58133e254af3da2ee514b46f5a532567bf5bb10
LuhemRevorg <148581376+LuhemRevorg@users.noreply.github.com>
2024-06-27 21:42:22 +0530

    Update settings.py

--- HestiaProject/settings.py
+++ HestiaProject/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 MEDIA_URL = '/media/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 STATICFILES_DIRS = [
