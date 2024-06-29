d999ec67d794b2917ee9604fe7dd7646176c1c3c
Mehul <mehulg8142@gmail.com>
2024-06-29 21:32:02 +0530

    y

--- HestiaProject/settings.py
+++ HestiaProject/settings.py
@@ -124,11 +124,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 MEDIA_URL = '/media/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT,'static')
+    os.path.join(BASE_DIR,'static')
 ]
 
 
